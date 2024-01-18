d37c7b0090c5bc53466881a7c7a23f36e8c6af65
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-01-18 21:29:03 +0530

    it starts

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div> twitter clone coming soon uwu</div>
+    </>
+  )
+}
+
+export default App
