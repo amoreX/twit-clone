bcbed8520c0aea798118b479dc9d1f997641d2b2
WahidX <wahidx93@gmail.com>
2024-01-20 21:04:04 +0530

    subscribe to premium

--- src/Components/rightsidebar.jsx
+++ src/Components/rightsidebar.jsx
@@ -1,9 +1,10 @@
 import Search from './Search'
+import Subs from './subs'
 export default function rightside(){
     return(
         <div id="rightside">
             <Search/>
-
+            <Subs/>
         </div>
     )
 }
