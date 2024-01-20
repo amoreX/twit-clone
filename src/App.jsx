18331c9cb62c8d9f9014adbf10835de5a502c547
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-01-20 17:39:53 +0530

    addednavbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Sidebar from './Components/Sidebar'
+import Nav from './Components/navbar'
+import Rside from './Components/rightsidebar'
 function App() {
 
   return (
     <>
-      <Sidebar />      
+      <div className='container'>
+      <Sidebar />  
+      <Nav />
+      <Rside />
+      </div>
+        
     </>
   )
 }
---
+++ src/Components/rightsidebar.jsx
@@ -0,0 +1,9 @@
+import Search from './Search'
+export default function rightside(){
+    return(
+        <div id="rightside">
+            <Search/>
+
+        </div>
+    )
+}
