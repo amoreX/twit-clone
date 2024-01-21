26fbcdff1e879b4c86775658f49de6bc59350c19
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-01-21 17:43:22 +0530

    added the tweet bar

---
+++ src/Components/Postfr.jsx
@@ -0,0 +1,10 @@
+export default function Postfr(){
+    return(
+        <div id="postt">
+            <div id="title">
+                <div id="actualname"> Ferocious kitty </div> 
+                <div id="username">@ferrykitty</div>
+            </div>
+        </div>
+    )
+}
