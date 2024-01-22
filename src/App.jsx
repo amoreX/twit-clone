24b0491f1c1c2fc256045c85171bfdaf91a36ec7
WahidX <wahidx93@gmail.com>
2024-01-22 18:10:22 +0530

    now also name

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import Sidebar from './Components/Sidebar'
 import Nav from './Components/navbar'
 import Rside from './Components/rightsidebar'
 import Postfr from './Components/Postfr'
 function App() {
+  const [username, setUsername] = useState('');
 
+  
+  const setUserName = () => {
+    const storedUsername = sessionStorage.getItem('username');
+
+    if (!storedUsername) {
+      const inputUsername = prompt('Enter your name:');
+      
+      if (inputUsername !== '') {
+        setUsername(inputUsername);
+        sessionStorage.setItem('username', inputUsername);
+      }
+      else{
+        const inputUsername = prompt('Nigga enter your fucking name:');
+      }
+      
+    } else {
+      
+      setUsername(storedUsername);
+    }
+  };
+  useEffect(() => {
+    setUserName();
+  }, []);
   return (
     <>
       <div className='container'>
