60aa9ae22d3edfbb86e1e268b85b1f6623db1533
WahidX <wahidx93@gmail.com>
2024-01-22 15:06:29 +0530

    connect database

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,6 +1,25 @@
+import axios from 'axios'
+import cors from 'cors'
+import { useState , useEffect } from 'react'
 import Posttfr from "./Postfr"
 import Mypost from "./Mypost"
 export default function nav(){
+    const [tweets, setTweets] = useState([]);
+    useEffect(() => {
+        // Fetch data from the backend when the component mounts
+        const fetchData = async () => {
+          try {
+            const response = await axios.get('https://twit-vtp0.onrender.com/tweets');
+            setTweets(response.data);
+          } catch (error) {
+            console.error('Error fetching data from the backend:', error);
+          }
+        };
+    
+        fetchData();
+      }, []);
+
+      
     return(
         < div id="navbar">
         <div id="nav">
