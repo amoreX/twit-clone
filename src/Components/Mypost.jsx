532625e085749252eadcf502e02a28a6271e31f2
WahidX <wahidx93@gmail.com>
2024-01-22 19:11:50 +0530

    pls work

--- src/Components/Mypost.jsx
+++ src/Components/Mypost.jsx
@@ -5,9 +5,16 @@ import cors from 'cors'
 export default function mypost(){
     const [tweetText, setTweetText] = useState('');
     const [username, setUsername] = useState(()=>{
-        return(
-        prompt('Enter your name:')
-        )
+        if(window.localStorage.getItem('username')){
+            return(
+                window.localStorage.getItem('username')
+            )
+        }
+        else{
+            const name=prompt("Enter name:");
+            window.localStorage.setItem('username',name);
+            return name;
+        }
     });
 
     const gal = (<svg viewBox="0 0 24 24" aria-hidden="true" className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-z80fyv r-19wmn03" style={{ color: 'rgb(29, 155, 240)' }}><g><path d="M3 5.5C3 4.119 4.119 3 5.5 3h13C19.881 3 21 4.119 21 5.5v13c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 21 3 19.881 3 18.5v-13zM5.5 5c-.276 0-.5.224-.5.5v9.086l3-3 3 3 5-5 3 3V5.5c0-.276-.224-.5-.5-.5h-13zM19 15.414l-3-3-5 5-3-3-3 3V18.5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-3.086zM9.75 7C8.784 7 8 7.784 8 8.75s.784 1.75 1.75 1.75 1.75-.784 1.75-1.75S10.716 7 9.75 7z"></path></g></svg>);
@@ -41,7 +48,7 @@ export default function mypost(){
         postData();
         setInterval(() => {
             window.location.reload();
-        },20000);
+        },2000);
 
     }
 
