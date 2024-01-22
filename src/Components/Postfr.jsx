ee662b9fb00b5ce758b24007da8c9171222c5147
WahidX <wahidx93@gmail.com>
2024-01-22 19:28:57 +0530

    it works :D

--- src/Components/Postfr.jsx
+++ src/Components/Postfr.jsx
@@ -9,7 +9,7 @@ export default function Postfr(props){
             <div id="title">
                 <div id="actualname"> {props.id} </div> 
                 <div id="username">@guest</div>
-                <div id="time">2hr ago</div>
+                <div id="time">{Math.floor(Math.random()*10)}hr ago</div>
             </div>
             <div id="content">
                 {props.children}  
