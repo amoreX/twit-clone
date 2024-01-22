9a4fb6b21ea5723bfbe8129ce26a5975c8fa05b8
WahidX <wahidx93@gmail.com>
2024-01-22 22:57:11 +0530

    bakchodi

--- src/Components/postbutton.jsx
+++ src/Components/postbutton.jsx
@@ -1,7 +1,7 @@
 export default function post(props){
 	return(
-		<div id="postb" >
-			Post
+		<div onClick={()=>{window.open("https://github.com/amoreX", "_blank")}}id="postb" >
+			{props.children}
 		</div>
 	)
 }
